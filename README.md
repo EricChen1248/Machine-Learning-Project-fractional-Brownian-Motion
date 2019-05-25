ffcf18a007ec3f084b6b5dd50ac09488666ad4a5
Eric Chen <ericchen1248@gmail.com>
2019-05-25 16:45:12 +0800

    Add requirements.txt, README, project.pdf

---
+++ README.md
@@ -0,0 +1,13 @@
+# Machine Learning Project 2019 - fractional Brownian Motion
+
+## Requirements
+````
+    python3.6+
+    pip install -r requirements.txt
+````
+
+## Setup
+Place all input data inside `./data`
+
+## License
+All code is licensed under the [MIT License](https://opensource.org/licenses/MIT)
--- convert_npy.py
+++ convert_npy.py
@@ -1,9 +1,14 @@
 import numpy as np
+import os
 
 FILES = ["./data/X_train.npz", "./data/Y_train.npz", "./data/X_test.npz"]
 
 for FILE in FILES:
+    NPY = FILE[:-3] + "npy"
+    if os.path.exists(NPY):
+        print(f"{NPY} already exists, skipping {FILE}")
+        continue
     print(f"Converting {FILE}")
     x = np.load(FILE, mmap_mode='r+')['arr_0']
-    np.save(FILE[:-3] + ".npy", x)
+    np.save(NPY, x)
     print(f"Finished converting {FILE}")
