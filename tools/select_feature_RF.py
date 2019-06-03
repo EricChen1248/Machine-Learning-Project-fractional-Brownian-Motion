2691389b3261c8b267e2e789b7cc2856ca61b074
Zetacat <k94155@gmail.com>
2019-06-03 21:07:03 +0800

    select_feature_RF.py functionalized.

--- tools/select_feature_RF.py
+++ tools/select_feature_RF.py
@@ -3,28 +3,61 @@ from sklearn.ensemble import RandomForestRegressor
 from joblib import dump, load
 import matplotlib.pyplot as plt
 
+VERBOSE = True
 DATA_DIR = 'C:/ml_data'
 MODEL_DIR = './model/RF'
 
 
-alpha = np.load(f'{DATA_DIR}/train_alpha.npy', mmap_mode='r')
-test_alpha = np.load(f'{DATA_DIR}/test_alpha.npy' , mmap_mode='r')
 
-for feature in range(3):
-    THRESH = 0.0001
-    rfRegressor = load(f'{MODEL_DIR}/randomForest{feature}.joblib')
-    importance = rfRegressor.feature_importances_
-    importance = np.where(importance > THRESH)
-    #print("Threshold",THRESH)
-    #importance=np.array(importance)
-    #print(importance.size)
-    train = np.load(f'{DATA_DIR}/X_train.npy').T[importance]
-    test = np.load(f'{DATA_DIR}/X_test.npy').T[importance]
-    train = train.T
-    test = test.T
-    #print(train.shape, alpha.shape)
-    #exit()
-    train = np.c_[train, alpha]
-    test = np.c_[test, test_alpha]
-    np.save(f'{DATA_DIR}/important{feature}_alpha.npy', train)
-    np.save(f'{DATA_DIR}/important_test{feature}_alpha.npy', test)
+def genImportantData(threshold = 0.0001):
+
+    print("Threshold:", threshold)
+
+    # load alpha
+    if VERBOSE:
+        print('loading pre-trained result: alpha...', end = '')
+    alpha = np.load(f'{DATA_DIR}/train_alpha.npy', mmap_mode='r')
+    test_alpha = np.load(f'{DATA_DIR}/test_alpha.npy' , mmap_mode='r')
+    if VERBOSE:
+        print('Done.')
+    
+    # load data
+    if VERBOSE:
+        print('loading origin data...', end = '')
+    X_train = np.load(f'{DATA_DIR}/X_train.npy')
+    X_test = np.load(f'{DATA_DIR}/X_test.npy')
+    if VERBOSE:
+        print('Done.')
+
+
+    for feature in range(3):
+        if VERBOSE:
+            print(f'generating feature {feature}...', end = '')
+
+        # load importance
+        THRESH = threshold
+        rfRegressor = load(f'{MODEL_DIR}/randomForest{feature}.joblib')
+        importance = rfRegressor.feature_importances_
+        importance = np.where(importance > THRESH)
+
+        train = X_train.T[importance]
+        test = X_test.T[importance]
+        
+        train = train.T
+        test = test.T
+        train = np.c_[train, alpha]
+        test = np.c_[test, test_alpha]
+
+        if VERBOSE:
+            print('saving...', end = '')
+        np.save(f'{DATA_DIR}/important{feature}_alpha.npy', train)
+        np.save(f'{DATA_DIR}/important_test{feature}_alpha.npy', test)
+        
+        if VERBOSE:
+            print('Done.')
+
+
+
+if __name__ == '__main__':
+    threshold = 0.001
+    genImportantData(threshold)
